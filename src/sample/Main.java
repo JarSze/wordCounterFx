e90a3bb67e9d9435addf8566c2636686de07e678
jaroslav szeligowski <j.szeligowski@gmail.com>
2018-03-24 14:25:15 +0100

    simple fx gui app which read and upload file content

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,10 @@
 package sample;
 
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,14 +15,19 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/MainWindow.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
 
+    boolean test(String s){
+        return  s.length() > 5;
+    }
+
     public static void main(String[] args) {
         launch(args);
+
     }
 }
---
+++ src/sample/controller/MainWindowController.java
@@ -0,0 +1,71 @@
+package sample.controller;
+
+/**
+ * Created by jszeligowski on 2018-03-24.
+ */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+
+public class MainWindowController {
+
+    @FXML
+    private BorderPane mainBorderPane;
+
+    @FXML
+    private TextArea resultTextArea;
+
+    public void readFromFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        File choosedFile = fileChooser.showOpenDialog(mainBorderPane.getScene().getWindow());
+
+        try {
+            List<String> strings = Files.readAllLines(Paths.get(choosedFile.getAbsolutePath()));
+            Map<String, Long> collect = strings.stream()
+                .flatMap(line -> Stream.of(line.split("\\s|\\.|\\, |\\. ")))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            StringBuilder sb = new StringBuilder();
+            collect.forEach((k, v) -> sb.append(k).append("----->").append(v).append("\n"));
+            resultTextArea.setText(sb.toString());
+
+        } catch (IOException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("File not found");
+            alert.setContentText("File doesn't exists. Choose correct file");
+            alert.showAndWait();
+        }
+
+    }
+
+    public void saveToFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showSaveDialog(mainBorderPane.getScene().getWindow());
+        String text = resultTextArea.getText();
+        // zapis do pliku
+
+        try {
+            Files.write(Paths.get(file.getAbsolutePath()), text.getBytes());
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setContentText("Zapis do pliku zakończony powodzeniem");
+            alert.showAndWait();
+        } catch (IOException e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setContentText("Zapis do pliku zakończony błędem. Wybierz inny plik");
+            alert.showAndWait();
+        }
+    }
+}
